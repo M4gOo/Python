8c831be646606336711ae1dafbb570fde5322969
M4gOo <57456345+M4gOo@users.noreply.github.com>
2023-07-30 16:32:26 +0100

    Create Banker Roulette_CONVERT-string-to-list_RANDOM.py

---
+++ HandsOn/Banker Roulette_CONVERT-string-to-list_RANDOM.py
@@ -0,0 +1,10 @@
+import random
+
+names = input("Give me everybody's names, seperated by a comma. ")
+
+#When the input names are separated by comma use this
+list_name = print("List of the names:",names.split(","))
+
+#When the input names are separated by space use this
+list_name = print("List of the names:",names.split())
+
