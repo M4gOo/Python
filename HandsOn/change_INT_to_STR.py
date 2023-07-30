55e7e9471e49689bed25952a00058f7488d7c1b8
M4gOo <57456345+M4gOo@users.noreply.github.com>
2023-07-30 16:08:57 +0100

    Update change_INT_to_STR.py

--- HandsOn/change_INT_to_STR.py
+++ HandsOn/change_INT_to_STR.py
@@ -2,9 +2,10 @@ variable_int = len(input("type any number \n"))
 change_variable_to_str = str(variable_int)
 print("The number has " + change_variable_to_str + " characters")
 
-type any number 
-23
-The number has 2 characters
+
+#type any number 
+#23
+#The number has 2 characters
 
 
 
@@ -12,5 +13,5 @@ calc = input("Type a two digit number: ")
 
 sum = print(int(calc[0]) + int(calc[1]))
 
-Type a two digit number: 45
-9
+#Type a two digit number: 45
+#9
