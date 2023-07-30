8ebfda7a5e8de90371fe8f01987ab76f0cc13fc1
M4gOo <57456345+M4gOo@users.noreply.github.com>
2023-07-30 17:12:34 +0100

    Update Banker Roulette_CONVERT-string-to-list_RANDOM-LEN.py

--- HandsOn/Banker Roulette_CONVERT-string-to-list_RANDOM-LEN.py
+++ HandsOn/Banker Roulette_CONVERT-string-to-list_RANDOM-LEN.py
@@ -1,10 +1,15 @@
+
 import random
 
-names = input("Give me everybody's names, seperated by a comma. ")
+# Split string method
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
 
-#When the input names are separated by comma use this
-list_name = print("List of the names:",names.split(","))
 
-#When the input names are separated by space use this
-list_name = print("List of the names:",names.split())
+num_items = len(names)
 
+random_choice = random.randint(0, num_items -1)
+
+who_will_pay = names [random_choice]
+
+print(f"{who_will_pay} is going to buy the meal today!")
